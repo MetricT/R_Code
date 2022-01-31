61248e3ac9d196a0405a5e6a07d6c4f4448a2e14
MetricT <MetricT@like.naw.dawg>
2022-01-31 17:21:31 -0600

    * Update the data to better reflect my latest version (not done yet...)

--- EconFinance/Num_Per_Inverted_Yield_Curves.R
+++ EconFinance/Num_Per_Inverted_Yield_Curves.R
@@ -27,13 +27,12 @@ library(scales)
 library(lubridate)
 library(forecast)
 library(cowplot)
+library(glue)
+library(TTR)
 
 # Set your FRED API key here.  You may request an API key at:
 # https://research.stlouisfed.org/useraccount/apikeys
-source("../Include/Recession_Bars.R")
-source("../Include/API_Keys.R")
-api_key_fred <- "PUT_YOUR_FRED_API_KEY_HERE"
-fredr_set_key(api_key_fred)
+fredr_set_key("PUT_YOUR_FRED_API_KEY_HERE")
 
 # Graph start date.   Earlier dates have fewer yield curves, so they will
 # appear shorter (top graph) or coarser (bottom graph).  For that reason, I
@@ -112,65 +111,38 @@ rates <-
 ### Compute the number of inverted yield curves
 ################################################################################
 
-all_yield_curves <-
+# Make a list of all the bond maturities we have data for
+bond_maturities <-
   rates %>%
-  mutate(
-    yc_30_20 = DGS30 - DGS20,
-    yc_30_10 = DGS30 - DGS10,
-    yc_30_7  = DGS30 - DGS7,
-    yc_30_5  = DGS30 - DGS5,
-    yc_30_3  = DGS30 - DGS3,
-    yc_30_2  = DGS30 - DGS2,
-    yc_30_1  = DGS30 - DGS1,
-    yc_30_6m = DGS30 - DGS6MO,
-    yc_30_3m = DGS30 - DGS3MO,
-    yc_30_1m = DGS30 - DGS1MO,
-    yc_20_10 = DGS20 - DGS10,
-    yc_20_7  = DGS20 - DGS7,
-    yc_20_5  = DGS20 - DGS5,
-    yc_20_3  = DGS20 - DGS3,
-    yc_20_2  = DGS20 - DGS2,
-    yc_20_1  = DGS20 - DGS1,
-    yc_20_6m = DGS20 - DGS6MO,
-    yc_20_3m = DGS20 - DGS3MO,
-    yc_20_1m = DGS20 - DGS1MO,
-    yc_10_7  = DGS10 - DGS7,
-    yc_10_5  = DGS10 - DGS5,
-    yc_10_3  = DGS10 - DGS3,
-    yc_10_2  = DGS10 - DGS2,
-    yc_10_1  = DGS10 - DGS1,
-    yc_10_6m = DGS10 - DGS6MO,
-    yc_10_3m = DGS10 - DGS3MO,
-    yc_10_1m = DGS10 - DGS1MO,
-    yc_7_5   = DGS7  - DGS5,
-    yc_7_3   = DGS7  - DGS3,
-    yc_7_2   = DGS7  - DGS2,
-    yc_7_1   = DGS7  - DGS1,
-    yc_7_6m  = DGS7  - DGS6MO,
-    yc_7_3m  = DGS7  - DGS3MO,
-    yc_7_1m  = DGS7  - DGS1MO,
-    yc_5_3   = DGS5  - DGS3,
-    yc_5_2   = DGS5  - DGS2,
-    yc_5_1   = DGS5  - DGS1,
-    yc_5_6m  = DGS5  - DGS6MO,
-    yc_5_3m  = DGS5  - DGS3MO,
-    yc_5_1m  = DGS5  - DGS1MO,
-    yc_3_2   = DGS3  - DGS2,
-    yc_3_1   = DGS3  - DGS1,
-    yc_3_6m  = DGS3  - DGS6MO,
-    yc_3_3m  = DGS3  - DGS3MO,
-    yc_3_1m  = DGS3  - DGS1MO,
-    yc_2_1   = DGS2  - DGS1,
-    yc_2_6m  = DGS2  - DGS6MO,
-    yc_2_3m  = DGS2  - DGS3MO,
-    yc_2_1m  = DGS2  - DGS1MO,
-    yc_1_6m  = DGS1  - DGS6MO,
-    yc_1_3m  = DGS1  - DGS3MO,
-    yc_1_1m  = DGS1  - DGS1MO,
-    yc_6m_3m = DGS6MO - DGS3MO,
-    yc_6m_1m = DGS6MO - DGS1MO,
-    yc_3m_1m = DGS3MO - DGS1MO
-  ) %>%
+  select(starts_with("DGS")) %>%
+  names() %>%
+  gsub("DGS", "", .) %>% rev()
+
+# Find every unique combination of maturities two at a time
+yield_curve_pairs <-
+  bond_maturities %>%
+  combn(2) %>%
+  t() %>%
+  as_tibble(.name_repair = "minimal") %>%
+  rename(high = 1, low = 2)
+
+# Pull our initial value of yield curves from the bond rates pulled from FRED
+all_yield_curves <- rates
+
+# Iterate over bond maturities and compute yield curve
+for (i in seq(1, nrow(yield_curve_pairs))) {
+  
+  high <- yield_curve_pairs %>% filter(row_number() == i) %>% pull("high")
+  low  <- yield_curve_pairs %>% filter(row_number() == i) %>% pull("low")
+  
+  all_yield_curves <-
+    all_yield_curves %>%
+    mutate("yc_{ high }_{ low }" := !!sym(glue("DGS{high}")) - 
+             !!sym(glue("DGS{low}")))
+}
+
+all_yield_curves <-
+  all_yield_curves %>%
   select(date, starts_with("yc")) %>%
   pivot_longer(-date, names_to = "maturity", values_to = "value") %>%
   filter(!is.na(value))
@@ -181,9 +153,6 @@ yield_curve_count <-
   group_by(date) %>%
   summarize(total_curves = n()) %>%
   ungroup()
-
-# If you want to see the # of yield curves over time
-#yield_curve_count %>% plot()
 
 # Count the number of inverted yield curves at a given date, and
 # compute the percent of yield curves inverted
@@ -212,10 +181,13 @@ g_num_inverts <-
   filter(date >= as.Date(graph_start)) %>%
   ggplot() +
   theme_bw() +
+  geom_line(data = yield_curve_count, aes(x = date, y = total_curves),
+            color = "darkred", linetype = "dashed") +
   geom_line(aes(x = date - (SMA_days - 1)/2, y = SMA(n_inverts, n = SMA_days))) +
   geom_recession_bars(as.Date(graph_start), as.Date(Sys.Date())) +
   scale_x_date(breaks = pretty_breaks(8)) +
-  labs(x = "", y = "", title = "Number of Inverted Yield Curves")
+  labs(x = "", y = "", title = "Number of Inverted Yield Curves", 
+       subtitle = "Red line = total number of yield curves as of that date")
 
 g_per_inverts <-
   all_inverts %>%
@@ -231,6 +203,7 @@ g_per_inverts <-
   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
                      breaks = pretty_breaks(5)) +
   labs(x = "", y = "",
-       title = "Number of Inverted Yield Curves as a % of All Yield Curves")
+       title = "Percent of all yield curves that are inverted", 
+       caption = "Data:  FRED [https://fred.stlouisfed.org]")
 
 print(plot_grid(g_num_inverts, g_per_inverts, nrow = 2, ncol = 1, align = "hv"))
